17eeead4ed02d080ce8deaddd59f7399fd84fea1
xiaogeCode <30351354+xiaogeCode@users.noreply.github.com>
2018-02-03 01:30:49 +0800

    小说爬虫

---
+++ readerSpider/src/rs/main/getAticles.java
@@ -0,0 +1,21 @@
+package rs.main;
+
+import rs.model.*;
+import rs.util.*;
+public class getAticles {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String firstUrl = "http://www.bxwx.org/b/5/5131/832882.html";
+        Article article = CommonUtil.getArticle(firstUrl);
+        while(article.getNextUrl() != null && article.getContent() != null && !article.getId().equals("996627")){
+            article = CommonUtil.getArticle(article.getNextUrl());
+            System.out.println(article.getId()+"----"+article.getTitle());
+        }
+	}
+
+}
---
+++ readerSpider/src/rs/model/Article.java
@@ -0,0 +1,52 @@
+package rs.model;
+
+public class Article {
+
+	private String id;//id
+    private String title;//标题
+    private String content;//内容
+    private String url;//当前章节url
+    private String nextUrl;//下一章url
+    
+    public void setId(String i) {
+		id = i;
+	}
+    public String  getId() {
+		return id;
+	}
+    
+    public void setTitle(String i) {
+		title = i;
+	}
+    public String  getTitle() {
+		return title;
+	}
+    
+    public void setContent(String i) {
+		content = i;
+	}
+    public String  getContent() {
+		return content;
+	}
+    
+    public void setUrl(String i) {
+		url = i;
+	}
+    public String  getUrl() {
+		return url;
+	}
+    
+    public void setNextUrl(String i) {
+    	nextUrl = i;
+	}
+    public String  getNextUrl() {
+		return nextUrl;
+	}
+    
+    @Override
+    public String toString() {
+        return "Article [id=" + id + ", title=" + title + ", content="
+                + content + ", url=" + url + ", nextUrl=" + nextUrl + "]";
+    }
+
+}
