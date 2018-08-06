a1aeeb4da52f3c92ce133878df2c752b9fce9322
xiaogeCode <30351354+xiaogeCode@users.noreply.github.com>
2018-08-06 20:04:51 +0800

    取水问题广度优先算法实现

---
+++ DistribuWater/src/cmd/MainFunc.java
@@ -0,0 +1,24 @@
+package cmd;
+
+import manager.DistribuManager;
+import model.StateModel;
+import manager.StateTree;
+import model.StateNode;
+
+/**
+ * Copyright (C), 2018-2018, XXX有限公司
+ * FileName: MainFunc
+ * Author:   xiaoge
+ * Date:     2018/8/6 12:57
+ * Description: ${DESCRIPTION}
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+public class MainFunc {
+    public static void main(String[] args) {
+        DistribuManager dm = new DistribuManager();
+        dm.serch();
+
+    }
+}
